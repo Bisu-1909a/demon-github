d53620ca44c1545139cbe4bdcf5bc4489526d7db
ojhabiswajeet84-rgb <ojhabiswajeet84@gmail.com>
2026-05-25 14:08:25 +0530

    updated test file

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-engjr
-klgbmgdrbfngfn[dndjf[
-    hnjdfonb
-    dbndfplbndflmb b;lfmbnpodj'b
-
-    dbnd
-    zb
-    dbndodbjz
-    fbniojb
-    Dbnaepdobjdfb
-     dzpn
-     rpobmsBjphorjob
-]]
+hello my name is Biswajeet Ojha.
+i want to be a data analyst.
+i am a good learner.
+my hoobbies are playing cricket and wTCHING ANIME.
+I AN GOOD TEAM PLAYER.
+i am a good problem solver .
+i am a good comunicator.
+i am good team player.
+Thank you for reading !
